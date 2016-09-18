2cb4deec0b372225004cbc54b67d1f2f0141b02f
hisaichi5518 <hisaichi5518@gmail.com>
2016-09-18 17:29:06 +0900

    [processor] プロセッサに必要な設定とメソッドを追加

--- processor/src/main/java/com/github/hisaichi5518/pusheventhandler/PushEventHandlerProcessor.java
+++ processor/src/main/java/com/github/hisaichi5518/pusheventhandler/PushEventHandlerProcessor.java
@@ -1,4 +1,23 @@
 package com.github.hisaichi5518.pusheventhandler;
 
-public class PushEventHandlerProcessor {
+import com.google.auto.service.AutoService;
+
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+
+@AutoService(Processor.class)
+@SupportedAnnotationTypes({"com.github.hisaichi5518.pusheventhandler.annotation.*"})
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+public class PushEventHandlerProcessor extends AbstractProcessor {
+    @Override
+    public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
+        return false;
+    }
 }
